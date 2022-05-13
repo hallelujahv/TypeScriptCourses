b514beae99f2697d33ed95c6c6c2ec90f81bf5c0
V <2583695112@qq.com>
2022-05-13 22:06:26 +0800

    es6语法

---
+++ es6/app.js
@@ -0,0 +1,25 @@
+"use strict";
+// Arrow Functions 箭头函数
+// 参数默认值
+const add = (n1, n2 = 1) => {
+    return n1 + n2;
+};
+console.log(add(1, 2));
+console.log(add(1));
+// 扩展运算符
+const person = {
+    name: "V",
+    age: 18,
+};
+// 第一条输出报错的原因，如果我要覆盖 name: "zky" 的话，那我干嘛还要写进去，所以就会提示报错
+// console.log({ name: "zky", ...person }); // error
+console.log(Object.assign(Object.assign({}, person), { name: "zky" }));
+function numSum(...args) {
+    return args.reduce((curVal, nextVal) => {
+        return curVal + nextVal;
+    }, 0);
+}
+console.log(numSum(1, 2, 3, 4, 5, 6));
+// 解构
+let { age } = person;
+console.log(age);
