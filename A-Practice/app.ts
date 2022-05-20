f811fa4bb773c7ffa03d96bbe9d4516e5aa4989f
V <2583695112@qq.com>
2022-05-20 20:03:43 +0800

    decorator

---
+++ A-Practice/app.ts
@@ -0,0 +1,69 @@
+function AutoBind(
+  _: any,
+  _2: string,
+  propertyDescriptor: PropertyDescriptor
+) {
+  const originalMethod = propertyDescriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      // console.log(this);
+      // this 指向 prototype
+      const fn = originalMethod.bind(this);
+      return fn;
+    }
+  }
+  return adjDescriptor;
+}
+
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
+  constructor() {
+    this.templateElement = <HTMLTemplateElement>(
+      document.getElementById("project-input")
+    );
+    this.hostElement = <HTMLDivElement>document.getElementById("app");
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+    this.titleInputElement = <HTMLInputElement>(
+      this.element.querySelector("#title")
+    );
+    this.descriptionInputElement = <HTMLInputElement>(
+      this.element.querySelector("#description")
+    );
+    this.peopleInputElement = <HTMLInputElement>(
+      this.element.querySelector("#people")
+    );
+
+    this.configure();
+    this.attach();
+  }
+
+  @AutoBind
+  private submitHandler(e: Event) {
+    e.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler);
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
+console.dir(prjInput);
